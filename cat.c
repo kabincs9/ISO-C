443174044295e0b3d2bf3e76a411e7cfcc506c01
kabincs9 <kabinbaniya9@gmail.com>
2026-05-18 23:54:34 +0545

    Add files via upload

---
+++ cat.c
@@ -0,0 +1,18 @@
+#include <cs50.h>
+#include <stdio.h>
+void meow(int n);
+
+int main(void)
+{
+    meow(5);
+}
+
+void meow(int n)
+{
+    for (int i=0; i<3; i++)
+
+      {
+    printf("meow\n");
+}
+
+}
